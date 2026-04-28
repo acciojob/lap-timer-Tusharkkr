2531303ca07af5eaab48c05a0096049a69b558cf
Tushar kumar <narutoboi41@gmail.com>
2026-04-28 12:15:00 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
-
 import React from "react";
 import './../styles/App.css';
+import LapTimer from "./LapTimer";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+        <LapTimer/>
     </div>
   )
 }
---
+++ src/components/LapTimer.js
@@ -0,0 +1,80 @@
+import React, { useEffect, useRef, useState } from "react";
+
+const LapTimer = () => {
+
+    let [sec, setSec] = useState('00')
+    let [min, setMin] = useState('00')
+    let [hour, setHour] = useState('00')
+    let [lap, setLap] = useState([])
+
+    let startRef = useRef()
+
+    function handleStart() {
+        startTime()
+    }
+
+    function startTime() {
+
+        startRef.current = setInterval(() => {
+
+            setSec(prev => {
+                if (prev < 9) {
+                    return `0${+prev + +1}`
+                } else if (prev < 60) {
+                    return +prev + +1
+                } else {
+                    setSec('00')
+                    setMin(prev => {
+                        if (prev < 9) {
+                            return `0${+prev + +1}`
+                        } else if (prev < 60) {
+                            return +prev + +1
+                        } else {
+                            setMin('00')
+                            setHour(prev => {
+                                if (prev < 9) {
+                                    return `0${+prev + +1}`
+                                } else if (prev < 24) {
+                                    return +prev + +1
+                                }
+                            })
+                        }
+                    })
+                }
+            })
+        }, 1)
+    }
+
+
+    function handleStop() {
+        clearInterval(startRef.current)
+    }
+
+    function handleReset() {
+        clearTimeout(startRef.current)
+        setLap([])
+        setSec('00')
+        setMin('00')
+        setHour('00')
+    }
+
+    function handleLap() {
+        let x = `${hour} : ${min} : ${sec}`
+        setLap([...lap, x])
+    }
+
+    return (
+        <div>
+            <p>{hour} : {min} : {sec}</p>
+            <button onClick={handleStart}>Start</button>
+            <button onClick={handleStop}>Stop</button>
+            <button onClick={handleLap}>Lap</button>
+            <button onClick={handleReset}>Reset</button>
+            {lap.map((value, index) => (
+                <p key={index}>{value}</p>
+            ))}
+        </div>
+    )
+}
+
+export default LapTimer
